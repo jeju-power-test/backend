7a48af3bc0d7e77a005941ac66636621c01717bc
KCH <doolysmile1@naver.com>
2022-11-17 00:48:28 +0900

    feat : DTO 구현

---
+++ src/main/java/com/goorm/jejuPowerTest/app/content/dto/ContentDTO.java
@@ -0,0 +1,13 @@
+package com.goorm.jejuPowerTest.app.content.dto;
+
+import lombok.*;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ContentDTO {
+    String answer;
+    String commentary;
+}
