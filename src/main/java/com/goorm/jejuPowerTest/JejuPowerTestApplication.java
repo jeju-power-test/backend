3c3bc97b79ba50acd77680aa624a0f79c843ab3c
KCH <doolysmile1@naver.com>
2022-11-16 17:34:26 +0900

    Initial settings

---
+++ src/main/java/com/goorm/jejuPowerTest/JejuPowerTestApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.jejuPowerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuPowerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuPowerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goorm/jejuPowerTest/JejuPowerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.jejuPowerTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuPowerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
