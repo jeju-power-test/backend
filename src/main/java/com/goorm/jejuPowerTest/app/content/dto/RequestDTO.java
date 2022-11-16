7f2af2fd3e1ac3ea384e9c5ca1ac003356946ed5
KCH <doolysmile1@naver.com>
2022-11-17 00:47:19 +0900

    feat : responseDTO

---
+++ src/main/java/com/goorm/jejuPowerTest/app/content/dto/RequestDTO.java
@@ -0,0 +1,13 @@
+package com.goorm.jejuPowerTest.app.content.dto;
+
+import lombok.*;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RequestDTO {
+    Long id;
+    boolean correct;
+}
